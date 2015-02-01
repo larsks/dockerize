1f9829bb266c6ec1e9ecbb19535f79a33cb45fb2
Lars Kellogg-Stedman <lars@redhat.com>
2015-01-31 22:20:02 -0500

    need rsync-compatible path specs

--- examples/thttpd.sh
+++ examples/thttpd.sh
@@ -3,7 +3,8 @@
 # This creates a minimal webserver using thttpd.
 
 dockerize -t dockerizeme/thttpd \
-	-a /var/www/thttpd /var/www \
+	-L copy-unsafe \
+	-a /var/www/thttpd/ /var/www/ \
 	-e '/usr/sbin/thttpd -D' \
 	-c '-d /var/www' \
 	/usr/sbin/thttpd
