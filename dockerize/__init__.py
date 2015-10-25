26a2cb45798ceb1c2d7e62b499a2ffbc44caaaac
six <brbsix@gmail.com>
2015-10-25 06:44:18 -0700

    Bump version to 0.2.2

--- dockerize/__init__.py
+++ dockerize/__init__.py
@@ -1,4 +1,4 @@
 __program__ = 'dockerize'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __description__ = 'A tool for creating minimal docker ' \
                   'images from dynamic ELF binaries.'
